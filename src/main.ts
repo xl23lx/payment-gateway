4e9475a8025150c6cce2e326fa0a8d507f4f9a6a
xl23lx <100032432+xl23lx@users.noreply.github.com>
2024-08-12 09:38:01 +0800

    Address CORS issue

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
