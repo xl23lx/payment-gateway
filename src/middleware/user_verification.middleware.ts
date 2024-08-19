8ed6810cc683c944185c80e3ae2816107f2a5f8f
xl23lx <100032432+xl23lx@users.noreply.github.com>
2024-08-19 09:53:36 +0800

    Fix middleware issues for checking of admin status

--- src/middleware/user_verification.middleware.ts
+++ src/middleware/user_verification.middleware.ts
@@ -3,6 +3,9 @@ import {Request,Response,NextFunction} from 'express';
 
 export function validateIfAdmin(req:Request,res:Response,next:NextFunction){
     let user:any=req.user;
+    if(!user){
+        throw new UnauthorizedException('You cannot perform this action');
+    }
     if(user.is_admin===false){
         throw new UnauthorizedException('You cannot perform this action');
     }
